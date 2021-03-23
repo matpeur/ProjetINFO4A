a23610e589979a894029659e95f389a144281942
matpeur <mathieu.bellegueulle@gmail.com>
2021-03-23 12:46:46 +0100

    ajout accesseurs et debut de deplacement

--- Creature.java
+++ Creature.java
@@ -1,14 +1,48 @@
 public abstract class Creature extends Thread
 {
   private String nom;
+  private int identifiant;
+  private Interface plateau;
 
-  public Creature(String s)
+  public int getIdentifiant()
+  {
+    return this.identifiant;
+  }
+
+  public void setIdentifiant(int id)
+  {
+    this.identifiant=id;
+  }
+
+  public Interface getInterface()
+  {
+    return this.plateau;
+  }
+
+  public void setInterface(Interface it)
+  {
+    plateau=it;
+  }
+
+  public Creature(String s, int id, Interface it)
   {
     nom=s;
+    setIdentifiant(id);
+    setInterface(it);
   }
 
-  public void capturer()
+  public abstract void run()
   {
 
   }
+
+  public synchronized void deplacement(int i)
+  {
+    switch(i){
+      case 1:gauche();
+      case 2:haut();
+      case 3:
+    }
+  }
+
 }
