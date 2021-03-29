9418147d055f0ba9d91b08708f5f836a57cfe323
Musa753 <80329531+Musa753@users.noreply.github.com>
2021-03-29 17:51:30 +0200

    Update Ennemis.java

--- Ennemis.java
+++ Ennemis.java
@@ -16,8 +16,8 @@ void setIndice(int i )
 {
    this.id=i;
 }
-int distmin(int id1 ,int id2 )
-{   int p=id1;
+/*int distmin(int id1 ,int id2 )
+{ int p=id1;
     int distance=0;
    do{
   
@@ -89,34 +89,101 @@ int distmin(int id1 ,int id2 )
  setIndice(p);
  return distance;
 
-}
+}*/
+int distmin(inti1,inti2)
+{​​​​​​​​    int col=I1.getColonne(i1);
+     int ligne=I1.getLigne(i1);
+     int col2=I1.getColonne(i2); 
+     int ligne2=I1.getLigne(i2);
+     int distance=0;
+do{​​​​​​​​
+if(Grille[getID(ligne+1,col)]==0)
+      {​​​​​​​​
+         distance=distance+1;
+         ligne=ligne+1;  
+ 
+      }​​​​​​​​  
+else
+    {​​​​​​​​  //printf("hello ");
+      if(Grille[getID(ligne,col+1)]==0)
+       {​​​​​​​​  
+         distance=distance+1;
+         col=col+1;  
+       }​​​​​​​​
+      else
+     {​​​​​​​​
+       if(0<ligne)
+         {​​​​​​​​
+           if(Grille[getID(ligne-1,col)]==0)
+              {​​​​​​​​  Grille[getID(ligne,col)]=2;
+                  distance=distance-1;
+                  ligne=ligne-1;  
+              }​​​​​​​​
+ 
+         }​​​​​​​​
+       else
+              {​​​​​​​​
+               if(0<col)
+                  {​​​​​​​​  if(Grille[getID(ligne,col-1)]==0)
+                     {​​​​​​​​
+                      Grille[getID(ligne,col)]=2;  
+                      col=col-1;
+                      distance=distance-1;
+                      Systeme.out.println("je vais marche arriere ");
+                     System.out.println(getID(ligne,col));
+                     }​​​​​​​​
+ 
+                  }​​​​​​​​
+               else
+                  {​​​​​​​​
+                      System.out.println("le labyrinthe n'est pas connexe");
+                  }​​​​​​​​
+ 
+              }​​​​​​​​
+
+
+ 
+      }​​​​​​​​
+
+
+
+ 
+    }​​​​​​​​
+
+ 
+  }​​​​​​​​ while(getID(ligne,col)!=i2);
+return distance;
+}​​​​​​​​
 
 void deplaceEnnemi(int id1,int id2)
 {    int p=id1;
      int icteur=100000000000;
       if(testMonte(id1)&&icteur>distmin(id1,id2))
       {
-        setIndice(getID(getLigne(id1)+1,getColonne(id1)));
-         icteur=distmin(p,id2);
+         setIndice(getID(getLigne(id1)+1,getColonne(id1)));
+         icteur=distmin(p,id2); 
       }
        if(testDescend(id1)&&icteur>distmin(getID((getLigne(id1)-1,getColonne(id1)),id2))
-      {
-
-        setIndice(p);
+      { 
+        
+        icteur=distmin(getID((getLigne(id1)-1,getColonne(id1)),id2);
         setIndice(getID(getLigne(id1)-1,getColonne(id1)));
-          icteur=distmin(getID((getLigne(id1)-1,getColonne(id1)),id2);
+        setIndice(p);
+         
       }
       if(testDroite(id1)&&icteur>distmin(getID(getLigne(id1),getColonne(id1)+1),id2))
-      { setIndice(p) ;
+      { 
+        icteur=distmin(getID((getLigne(id1),getColonne(id1)+1),id2),id2);
+        setIndice(p) ;
         setIndice(getID(getLigne(id1),getColonne(id1)+1));
-         icteur=distmin(getID((getLigne(id1),getColonne(id1)+1),id2),id2);
+         
       }
        if(testGauche(id1)&&icteur>distmin(getID(getLigne(id1),getColonne(id1)-1),id2))
       {
-
+        icteur=distmin(getID(getLigne(id1),getColonne(id1)-1),id2);
         setIndice(p) ;
         setIndice(getID(getLigne(id1),getColonne(id1)-1));
-        icteur=distmin(getID(getLigne(id1),getColonne(id1)-1),id2);
+       
       }
 }
 
