ccf85d01a891f66aa4991915acf239f6c955a9d0
matpeur <78799819+matpeur@users.noreply.github.com>
2021-03-29 10:02:23 +0200

    Update Debut.java

--- Debut.java
+++ Debut.java
@@ -1,19 +1,18 @@
-public class Debut{
+public class Debut
+{
   public static void main(String[] args)
   {
     Moteur m=new Moteur();
     m.ajoutCuisinier("Michel");
     m.visualisation();
     boolean arret=false; //fin du jeu
-    //while(!arret)
+    int i=0;
+    while(i<10)
       {
-        //m.visualisation();
-        try
-        {
-          //efface la console
-        }catch(Exception e){e.printStackTrace();}
+        System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        m.visualisation();
+        i++;
+
       }
   }
-
-
 }
