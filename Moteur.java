1e212af500490b6d3275457fd3a261d5d0dbabbb
Musa753 <80329531+Musa753@users.noreply.github.com>
2021-04-05 23:14:30 +0200

    Update Moteur.java

--- Moteur.java
+++ Moteur.java
@@ -4,8 +4,9 @@ public class Moteur
 {
   public Plateau plateau;
   private ArrayList<Creature> creature;
-  //public ArrayListe<Burger> burger;
-
+  public ArrayListe<Burger> Burgers;
+  public ArrayListe<Elements> Cmp;
+  int n;
   public int getNbJoueur()
   {
     return creature.size();
@@ -15,7 +16,9 @@ public class Moteur
   {
     plateau=new Plateau();
     creature = new ArrayList<Creature>();
-    //burger = new ArrayList<Burger>();
+    Burgers = new ArrayList<Burger>();
+    Cmp=new ArrayList<Elements>();
+    n=1;
   }
 
   public void ajoutJoueur(String Nom)
@@ -23,7 +26,67 @@ public class Moteur
     Joueur j=new Joueur(plateau, Nom, getNbJoueur(), ' ');
     creature.add((Creature)j);
   }
-
+  
+  
+public void ajoutBurger(Burger B)
+{   B=new Burger(Joueur j,10, plateau);//je ne sais pas koi mettre comme premier parametre du constructeur "joueur courant"
+	Burgers.add(B);
+}
+public void ajoutComposants(Elements e)
+{
+   if(i%3<=1)
+   {  if(i==1)
+	  {   e= new Elements("Burger1PainDessous",5,Burgers.get(0),plateau) ;
+		  cmp.add((PainDessous)e);
+		  
+	   }
+	  if(i==2)
+	   {   e= new Elements("Burger1Salade",5,Burgers.get(0),plateau)  ;
+		   cmp.add((SaladeViandeetc)e);
+	   
+	   }
+	  if(i==3)
+	   {     e= new Elements("Burger1PainDessus",5,Burgers.get(0),plateau);  
+		     cmp.add((PainDessus)e);
+		}
+	}	
+	if(1<i%3<=2)
+   {
+	    if(i==4)
+	  {   e= new Elements("Burger2PainDessous",5,Burgers.get(1),plateau) ;
+		  cmp.add((PainDessous)e);
+		  
+	   }
+	  if(i==5)
+	   {   e= new Elements("Burger2Salade",5,Burgers.get(1),plateau)  ;
+		   cmp.add((SaladeViandeetc)e);
+	   
+	   }
+	  if(i==6)
+	   {     e= new Elements("Burger2PainDessus",5,Burgers.get(1),plateau);  
+		     cmp.add((PainDessus)e);
+		}
+   }
+	if(2<i%3<=3)
+	{
+	   if(i==7)
+	  {   e= new Elements("Burger3PainDessous",5,Burgers.get(2),plateau) ;
+		  cmp.add((PainDessous)e);
+		  
+	   }
+	  if(i==8)
+	   {   e= new Elements("Burger3Salade",5,Burgers.get(2),plateau)  ;
+		   cmp.add((SaladeViandeetc)e);
+	   
+	   }
+	  if(i==9)
+	   {     e= new Elements("Burger3PainDessus",5,Burgers.get(2),plateau);  
+		     cmp.add((PainDessus)e);
+		}
+	}
+	i++;
+ //stp pourrais tu placé les composants du burger dans le plateau,en gros remplacé les 5 par leur position initiales
+}
   public void ajoutCuisinier(String Nom)
   {
     Cuisinier c=new Cuisinier(plateau, Nom, getNbJoueur());
@@ -51,7 +114,8 @@ public class Moteur
     }
     return r;
   }
-
+//pour la visualisation des burgers j'ai fait une fonction affiche() dans Burger mais je pense ke c'est pas suffisant  
+// car il faudra toute affiché les burgers meme vide et les composants sur le plateau   donc je te laisse l'inserer dans celle ci
   public void visualisation()
   {
     for (int i=0; i<plateau.getTaille(); i++)
