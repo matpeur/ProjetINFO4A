5eee9fcf81c9d87322c957d7fca6e61786a4aac8
matpeur <78799819+matpeur@users.noreply.github.com>
2021-04-07 10:15:42 +0200

    Update Debut.java

--- Debut.java
+++ Debut.java
@@ -5,10 +5,33 @@ public class Debut{
     m.ajoutCuisinier("Michel");
     m.getCreature(0).setPlace(4*m.getPlateau().getNbColonne()+15);
     m.visualisation();
-    m.start();
-    boolean arret=false; //fin du jeu
+    System.out.println("Quel adresse ?");
+    String s="";
+    while(!s.equals(""))
+    {
+      BufferedReader saisie=new BufferedReader(new InputStreamReader(System.in));
+      s =(String)saisie.readLine();
+    }
+    if(s.equals("server"))
+    {
+      Server s = new Server(m);
+      s.start();
+    }
+    else
+    {
+      Client c= new Client();
+      c.start();
+      while(c.getMoteur()==null)
+      {
 
-    while(!arret)
+      }
+      System.out.println("Fichier reçu");
+      System.out.println(c.getMoteur().getCreature().getNom());
+    }
+    //m.start();
+    //boolean arret=false; //fin du jeu
+
+    /*while(!arret)
       {
         m.visualisation();
         try
@@ -19,6 +42,7 @@ public class Debut{
           {}
         }catch(Exception e){e.printStackTrace();}
       }
+      */
   }
 
 
