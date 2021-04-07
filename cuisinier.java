f585d83e4a61a860d5869419fd1b1e7adf5bbcca
Musa753 <80329531+Musa753@users.noreply.github.com>
2021-04-07 14:22:21 +0200

    Update cuisinier.java

--- cuisinier.java
+++ cuisinier.java
@@ -1,20 +1,29 @@
-import java.io.*;
-public class Cuisinier extends Joueur implements Serializable
+public class Cuisinier extends Joueur
 {
   boolean poivre ;
   int vie;
-
+   
   public Cuisinier (Moteur m, String nom, int id)
   {
    super(m, nom, id, 'C');
-   this.poivre = true;
+   this.poivre = false;
    this.vie=3;
+    
   }
 
 
-  public void poivrer()
+  public void  poivrer()
   {
-
+     Plateau I=m.getplateau();
+     int L=I.getLigne(this.id);
+     int C=I.getColonne(this.id);
+     boolean a=(super.getCotechoisi()==0&&(I.getIndice[L-1,C]==2));
+     boolean b=(super.getCotechoisi()==1&&(I.getIndice[L,C-1]==2));
+     boolean c=(super.getCotechoisi()==2&&( I.getIndice[L+1,C]==2));
+     boolean d=(super.getCotechoisi()==3&&(I.getIndice[L,C-1]==2));// 2  car toutefois  on mettra à 2 la case occupée par un ennemi 
+     if(a||b||c||d)    
+           poivre=true;
+  
   }
 
   public void mort()
@@ -23,3 +32,4 @@ public class Cuisinier extends Joueur implements Serializable
     vie--;
   }
 }
+
