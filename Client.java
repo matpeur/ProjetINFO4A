355e9c776ee01fb61aa9fc439598aee352d6e31e
matpeur <78799819+matpeur@users.noreply.github.com>
2021-04-07 08:11:41 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,5 +1,19 @@
 import java.io.*;
 
-public class Client{
+public class Client
+{
+  static  final  int  port = 8080;
 
+  public  static  void  main(String [] args) throws  Exception
+  {
+    Socket  socket = new  Socket(args[0], port);
+    System.out.println("SOCKET = " + socket);
+    ObjectOutputStream  oss = new  ObjectOutputStream(socket.getOutputStream ());
+    oss.writeObject(new  Moteur());
+    System.out.println("END");
+    //  attention  aux  commandes  de type  protocole
+    oss.writeObject("END") ;
+    oss.close();
+    socket.close();
+  }
 }
