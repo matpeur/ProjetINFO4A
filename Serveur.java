d43cbcd4987015f95b2543aec17aa3a7954543b6
matpeur <78799819+matpeur@users.noreply.github.com>
2021-04-07 08:09:57 +0200

    Update Serveur.java

--- Serveur.java
+++ Serveur.java
@@ -1,4 +1,19 @@
+import java.io.*;
 public class Serveur
 {
-  
+  static  final  int  port = 8080;
+
+  public  static  void  main(String [] args) throws  Exception
+  {
+    ServerSocket s = new  ServerSocket(port);
+    Socket  soc = s.accept ();
+    ObjectInputStream  ois =   new  ObjectInputStream(soc.getInputStream ());
+    while (true)
+    {
+      Object o = ois.readObject ();
+      if (o.equals("END")) break;
+    }
+    ois.close();
+    soc.close();
+  }
 }
