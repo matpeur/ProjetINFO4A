b5754039ca10bb1ff12c32f72188fdf291eccf55
matpeur <78799819+matpeur@users.noreply.github.com>
2021-04-07 08:53:46 +0200

    Update Serveur.java

--- Serveur.java
+++ Serveur.java
@@ -2,9 +2,27 @@ import java.io.*;
 public class Serveur
 {
   static  final  int  port = 8080;
+  private synchronized Moteur moteur;
+
+  public Serveur( Moteur m)
+  {
+    moteur = m;
+  }
 
   public  static  void  main(String [] args) throws  Exception
   {
+    ObjectOutputStream  oss = new  ObjectOutputStream(socket.getOutputStream ());
+    while (true)
+    {
+      Object o = ois.readObject ();
+      if(o.equals("MOTEUR"))
+      {
+        oss.writeObject(m);
+      }
+      if (o.equals("END")) break;
+    }
+    oss.close();
+    socket.close();
     ServerSocket s = new  ServerSocket(port);
     Socket  soc = s.accept ();
     ObjectInputStream  ois =   new  ObjectInputStream(soc.getInputStream ());
