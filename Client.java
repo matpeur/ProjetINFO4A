c6de5f1b3129e20afd77b2c00f8285d19c4f9b84
matpeur <78799819+matpeur@users.noreply.github.com>
2021-04-07 08:53:44 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -7,13 +7,38 @@ public class Client
   public  static  void  main(String [] args) throws  Exception
   {
     Socket  socket = new  Socket(args[0], port);
+    ObjectOutputStream  oss = new  ObjectOutputStream(socket.getOutputStream ());
+    oss.writeObject(new  Moteur());
+    //  attention  aux  commandes  de type  protocole
+    oss.writeObject("MOTEUR") ;
+    while (true)
+    {
+      Object o = ois.readObject ();
+      if (o.equals("END")) break;
+    }
+    oss.close();
+    socket.close();
+    Socket  socket = new  Socket(args[0], port);
     System.out.println("SOCKET = " + socket);
     ObjectOutputStream  oss = new  ObjectOutputStream(socket.getOutputStream ());
     oss.writeObject(new  Moteur());
     System.out.println("END");
     //  attention  aux  commandes  de type  protocole
     oss.writeObject("END") ;
+    while (true)
+    {
+      Object o = ois.readObject ();
+      if (o.equals("END")) break;
+    }
     oss.close();
     socket.close();
+    ObjectInputStream  ois =   new  ObjectInputStream(soc.getInputStream ());
+    while (true)
+    {
+      Object o = ois.readObject ();
+      if (o.equals("END")) break;
+    }
+    ois.close();
+    soc.close();
   }
 }
