ad059540a3b57a03c0a20746957652d76f1a5254
matpeur <78799819+matpeur@users.noreply.github.com>
2021-04-07 09:21:10 +0200

    Update Serveur.java

--- Serveur.java
+++ Serveur.java
@@ -1,7 +1,7 @@
 import java.io.*;
-public class Serveur
+public class Serveur extends Thread
 {
-  static  final  int  port = 8080;
+  public int  port = 8080;
   private synchronized Moteur moteur;
 
   public Serveur( Moteur m)
@@ -9,29 +9,42 @@ public class Serveur
     moteur = m;
   }
 
-  public  static  void  main(String [] args) throws  Exception
+  public void setMoteur(Moteur m)
   {
-    ObjectOutputStream  oss = new  ObjectOutputStream(socket.getOutputStream ());
-    while (true)
+    while(!m.tryLock())
     {
-      Object o = ois.readObject ();
-      if(o.equals("MOTEUR"))
+      m.lock();
+      this.moteur = m;
+      m.unlock();
+    }
+  }
+
+  public void  run()
+  {
+    try
+    {
+      ServerSocket s = new  ServerSocket(port);
+      Socket  soc = s.accept ();
+      ObjectOutputStream  oss = new  ObjectOutputStream(soc.getOutputStream ());
+      ObjectInputStream  ois =   new  ObjectInputStream(soc.getInputStream ());
+      while (true)
       {
-        oss.writeObject(m);
+        Object o = ois.readObject ();
+        if(o.equals("MOTEUR"))
+        {
+          while(!m.tryLock())
+          {
+          m.Lock();
+          oss.writeObject(m);
+          m.unLock();
+          }
+        }
+        if (o.equals("END")) break;
       }
-      if (o.equals("END")) break;
+      oss.close();
+      ois.close();
+      soc.close();
     }
-    oss.close();
-    socket.close();
-    ServerSocket s = new  ServerSocket(port);
-    Socket  soc = s.accept ();
-    ObjectInputStream  ois =   new  ObjectInputStream(soc.getInputStream ());
-    while (true)
-    {
-      Object o = ois.readObject ();
-      if (o.equals("END")) break;
-    }
-    ois.close();
-    soc.close();
+    catch(Execption e){e.printStackTrace();}
   }
 }
