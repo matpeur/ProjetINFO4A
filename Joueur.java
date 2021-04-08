7480959304d46ac31a7b6b188cd07eb779504f38
Musa753 <80329531+Musa753@users.noreply.github.com>
2021-04-08 14:01:57 +0200

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -1,29 +1,28 @@
 import java.io.*;
 
 
-public abstract class Joueur extends Creature implements Serializable
+public abstract class Joueur extends Creature
 {
   char[] commandes;
-
-   int cotechoisi;
+   
 
  public Joueur(Moteur m,String nom, int id, char symbole)
  {
    super(nom, id, m);
-   commandes = new char[]{'z', 'q', 's', 'd'};
-   cotechoisi=0;
+   commandes = new char[]{'z', 'q', 's', 'd','P'};
+ 
    super.setSymbole(symbole);
  }
 
-
-
+  public int getPlace(){return super.getPlace();}
+  public Moteur getMoteur(){return this.m;}
 
   public String toString()
   {
     return "Joueur: "+super.getNom();
   }
 
- @Override
+  @Override
   public void run()
   {
     while(true)
@@ -35,28 +34,25 @@ public abstract class Joueur extends Creature implements Serializable
         if(commande==commandes[0])
           {
             deplaceHaut();
-            cotechoisi=0;
           }
           else if(commande==commandes[1])
           {
             deplaceGauche();
-             cotechoisi=1;
           }
           else if(commande==commandes[2])
           {
             deplaceBas();
-             cotechoisi=2;
+          
           }
           else if(commande==commandes[3])
           {
             deplaceDroite();
-             cotechoisi=3;
           }
         }
         catch(IOException e){e.printStackTrace();}
     }
   }
-  public int Cotechoisi() {return this.cotechoisi;}
+  
   public abstract void mort();
 
 }
