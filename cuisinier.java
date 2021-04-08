00d730f8a688a14db6f4d2d612ede1336f7959c5
matpeur <78799819+matpeur@users.noreply.github.com>
2021-04-08 12:12:03 +0200

    Update cuisinier.java

--- cuisinier.java
+++ cuisinier.java
@@ -2,13 +2,13 @@ public class Cuisinier extends Joueur
 {
   boolean poivre ;
   int vie;
-   
+
   public Cuisinier (Moteur m, String nom, int id)
   {
    super(m, nom, id, 'C');
    this.poivre = false;
    this.vie=3;
-    
+
   }
 
 
@@ -20,10 +20,10 @@ public class Cuisinier extends Joueur
      boolean a=(super.getCotechoisi()==0&&(I.getIndice[L-1,C]==2));
      boolean b=(super.getCotechoisi()==1&&(I.getIndice[L,C-1]==2));
      boolean c=(super.getCotechoisi()==2&&( I.getIndice[L+1,C]==2));
-     boolean d=(super.getCotechoisi()==3&&(I.getIndice[L,C-1]==2));// 2  car toutefois  on mettra à 2 la case occupée par un ennemi 
-     if(a||b||c||d)    
+     boolean d=(super.getCotechoisi()==3&&(I.getIndice[L,C+1]==2));// 2  car toutefois  on mettra à 2 la case occupée par un ennemi
+     if(a||b||c||d)
            poivre=true;
-  
+
   }
 
   public void mort()
@@ -32,4 +32,3 @@ public class Cuisinier extends Joueur
     vie--;
   }
 }
-
